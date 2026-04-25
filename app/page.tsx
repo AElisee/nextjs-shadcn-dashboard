0a94d1edd86f90f14887080379df5fa61a485b77
AElisee <2kange.eli@gmail.com>
2026-04-25 04:03:05 +0000

    cardList

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import CardList from "@/components/CardList";
 import { Appareachart } from "@/components/ui/Appareachart";
 import AppBarchart from "@/components/ui/AppBarchart";
 import { Apppiechart } from "@/components/ui/Apppiechart";
@@ -9,14 +10,19 @@ export default function Home() {
         <AppBarchart />
       </div>
       <div className="bg-primary-foreground p-4 round-lg">test </div>
-      <div className="bg-primary-foreground p-4 round-lg"></div>
+      <div className="bg-primary-foreground p-4 round-lg">
+        <CardList title="Latest Transactions" />
+      </div>
       <div className="bg-primary-foreground p-4 round-lg">
         <Apppiechart />
       </div>
       <div className="bg-primary-foreground p-4 round-lg lg:col-span-2 xl:col-span-1 2xl:col-span-2">
         <Appareachart />
       </div>
-      <div className="bg-primary-foreground p-4 round-lg">test </div>
+      <div className="bg-primary-foreground p-4 round-lg">
+        {" "}
+        <CardList title="Popular Content" />{" "}
+      </div>
     </div>
   );
 }
